14c6bc54e23b1350499d118bd7a755949d3c415e
Serge <serge.hendrickx@fourcast.io>
2018-11-04 03:10:56 +0100

    added file gen for flask

--- flask-server/main.py
+++ flask-server/main.py
@@ -1,12 +1,21 @@
-from flask import Flask
-import subprocess
+from flask import Flask, request, jsonify
+import subprocess, requests, json
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    res = requests.post('http://localhost:8080/goToBlockchain', json={"mytext":"lalala"})
+    if res.ok:
+        print res.json()
     return 'Hello, World!'
 
+
+@app.route('/goToBlockchain', methods=['GET', 'POST'])
+def go_to_chain():
+    content = request.json
+    write_to_file(request.json)
+    return jsonify(content)
 
 
 @app.route('/status/')
@@ -19,6 +28,19 @@ def get_status():
         return e
         return "An error occurred while trying to fetch task status updates."
 
-#
+
+def write_to_file(args):
+    with open("settings.py", "w") as f:
+        f.write("ENABLE_AUTO_INVOKE = True \n")
+        for arg in args:
+            f.write(arg + '= ' + json.dumps((args[arg])) + '\n')
+
+    #
 # if __name__ == '__main__':
 #     app.run()
+
+
+# HARDCODED_WALLETNAME = 'testwallet'
+# HARDCODED_PASSWORD = '1234567890'
+# HARDCODED_ARGS = ['0x1d36641faca64ddd0f49af488e543a0f89860690', "add", "1", "2"]
+# ENABLE_AUTO_INVOKE = True
--- flask-server/run.sh
+++ flask-server/run.sh
@@ -7,7 +7,6 @@ python --version
 cd neo-python
 source venv/bin/activate
 python --version
-#python ../flask-server/fake_tty.py
 echo "START"
 np-prompt -p | cat
 echo "END"
