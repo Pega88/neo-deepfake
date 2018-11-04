e9c9bdaa1dd169c0fd08cdaa038087a529005282
Niels Buekers <niels.buekers@gmail.com>
2018-11-04 14:54:11 +0100

    main

--- flask-server/main.py
+++ flask-server/main.py
@@ -3,6 +3,7 @@ import subprocess, requests, json
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     # res = requests.post('http://localhost:8080/goToBlockchain', json={"mytext":"lalala"})
@@ -11,17 +12,14 @@ def hello_world():
     return 'Hello, World!'
 
 
-@app.route('/goToBlockchain', methods=['GET', 'POST'])
-def go_to_chain():
-    content = request.json
+@app.route('/deploy', methods=['POST'])
+def deploy():
     write_to_file(request.json)
-    return jsonify(content)
-
-
-@app.route('/status/')
-def get_status():
     try:
-        result = subprocess.check_output(['./run.sh'], shell=False)
+        result = subprocess.check_output(['./run.sh'], shell=False).decode("utf-8")
+        #result = [line for line in result.split("\n") if str(line).startswith("Result")][0][7:]
+        #result = result.replace('\'', '"')
+        #print(result)
         return result
     except subprocess.CalledProcessError as e:
         return e
@@ -33,5 +31,3 @@ def write_to_file(args):
         f.write("ENABLE_AUTO_INVOKE = True \n")
         for arg in args:
             f.write(arg + '= ' + json.dumps((args[arg])) + '\n')
-
-
